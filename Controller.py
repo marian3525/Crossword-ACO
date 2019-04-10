7c8613cfd1fcd2cf385355355c8201cf09b5bf14
marian <marian3525@yahoo.com>
2019-04-11 01:27:46 +0300

    init

--- Controller.py
+++ Controller.py
@@ -5,6 +5,15 @@ class Controller:
         # matrix
         self.__trace = [[]]
 
+        # run parameters
+        self.alpha = 0
+        self.beta = 0
+        self.noAnts = 0
+        # iterations per epoch
+        self.noIterations = 0
+        # the number of times the simulation is restarteds
+        self.noEpochs = 0
+
     def iteration(self):
         pass
 
@@ -14,5 +23,20 @@ class Controller:
     def runAlgo(self):
         pass
 
-    def loadParameters(self):
-        pass
+    """
+    Run parameters are read from file
+    """
+    def loadParameters(self, filename):
+        f = open(filename, "r")
+
+        self.alpha = int(f.readline().split("=")[1])
+
+        self.beta = int(f.readline().split("=")[1])
+
+        self.noAnts = int(f.readline().split("=")[1])
+
+        self.noEpochs = int(f.readline().split("=")[1])
+
+        self.noIterations = int(f.readline().split("=")[1])
+
+        f.close()
